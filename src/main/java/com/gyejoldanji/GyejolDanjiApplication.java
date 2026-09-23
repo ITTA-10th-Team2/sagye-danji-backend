497cc4591ba8a9402f299ffa150b574725537d9d
Juwoojae <juwqq1234@naver.com>
2026-09-23 23:30:37 +0900

    init: 프로젝트 초기설정

---
+++ src/main/java/com/gyejoldanji/GyejolDanjiApplication.java
@@ -0,0 +1,13 @@
+package com.gyejoldanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyejolDanjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyejolDanjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyejoldanji/GyejolDanjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyejoldanji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyejolDanjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
